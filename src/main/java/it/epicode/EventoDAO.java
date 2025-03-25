0a253b00f8c4a262063ac0f61006e7aa64c5ea5f
“Cosimo <pastore_cosimo@yahoo.it>
2025-03-25 17:54:59 +0100

    Fase 2 completata, es 1

--- src/main/java/it/epicode/EventoDAO.java
+++ src/main/java/it/epicode/EventoDAO.java
@@ -1,5 +1,28 @@
 package it.epicode;
 
+import jakarta.persistence.EntityManager;
+
+import javax.swing.text.html.parser.Entity;
+
 public class EventoDAO {
+    private EntityManager em;
+
+    public EventoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Evento evento) {
+        em.persist(evento);
+    }
+
+    public Evento getById(Long id) {
+        return em.find(Evento.class, id);
+    }
+
+    public void delete(Evento evento) {
+        em.remove(evento);
+    }
+
+
 
 }
--- src/main/java/it/epicode/Main.java
+++ src/main/java/it/epicode/Main.java
@@ -1,14 +1,14 @@
 package it.epicode;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
+
+
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
-        EntityManager em = emf.createEntityManager();
-        Evento evento = new Evento();
+        //EntityManager em = emf.createEntityManager();
+        //Evento evento = new Evento();
     }
 }
