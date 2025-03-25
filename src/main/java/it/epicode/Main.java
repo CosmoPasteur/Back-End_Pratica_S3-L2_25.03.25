5fc3fa33d39a6279e248715e1a6a8c3c29843fd4
“Cosimo <pastore_cosimo@yahoo.it>
2025-03-25 17:21:43 +0100

    Fase 2 completata, es 1

--- src/main/java/it/epicode/Main.java
+++ src/main/java/it/epicode/Main.java
@@ -1,4 +1,14 @@
 package it.epicode;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
+        EntityManager em = emf.createEntityManager();
+        Evento evento = new Evento();
+    }
 }
